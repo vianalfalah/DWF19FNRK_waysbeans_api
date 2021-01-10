3ca6467398f7d1750d0e074e1109683369554cf2
Your Name <yourmail@gmail.com>
2021-01-10 23:26:28 +0700

    deploy

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,8 +5,7 @@ const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
-
-    const {body} = req.body,
+    const { body } = req.body;
     const schema = Joi.object({
       fullName: Joi.string().min(5).required(),
       email: Joi.string().email().min(4).required(),
@@ -18,7 +17,7 @@ exports.register = async (req, res) => {
     });
     console.log(req.body);
     const checkUser = await User.findOne({
-      where: { email: body.email},
+      where: { email: body.email },
     });
     console.log("check", checkUser);
     if (checkUser) {
